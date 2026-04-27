a8b2e15e7169a891764a45018a8b9d9e625c1d18
knoffelcut <hardus_richter@hotmail.com>
2026-04-27 19:55:29 +0200

    Nanodet Plus support

--- frigate/detectors/plugins/onnx.py
+++ frigate/detectors/plugins/onnx.py
@@ -12,6 +12,7 @@ from frigate.detectors.detector_config import (
 )
 from frigate.util.model import (
     post_process_dfine,
+    post_process_nanodet_plus,
     post_process_rfdetr,
     post_process_yolo,
     post_process_yolox,
@@ -98,6 +99,12 @@ class ONNXDetector(DetectionApi):
                 self.height,
                 self.grids,
                 self.expanded_strides,
+            )
+        elif self.onnx_model_type == ModelTypeEnum.nanodet_plus:
+            return post_process_nanodet_plus(
+                tensor_output[0],
+                self.width,
+                self.height,
             )
         else:
             raise Exception(
